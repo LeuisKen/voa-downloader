71b57adcca0c825df16a624c145029c4845fd02a
LeuisKen <leuisken@gmail.com>
2017-05-28 18:12:20 +0800

    feat:American Story 下载

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const fs = require('fs');
+const fetch = require('isomorphic-fetch');
+const cheerio = require('cheerio');
+
+const host = 'http://www.51voa.com/';
+
+async function getText(url) {
+    return fetch(url).then(res => res.text());
+}
+
+async function getBuffer(url) {
+    return fetch(url).then(res => res.buffer());
+}
+
+async function main() {
+    let audioList = [];
+    for (let i = 1; i <= 7; i++) {
+        let res = await getText(`${host}/American_Stories_${i}.html`);
+        let $ = cheerio.load(res);
+        let list = $('#list').find('ul li a');
+        for (let i = 0; i < list.length; i++) {
+            let title = $(list[i]).text().trim();
+            let article = await getText(`${host}${$(list[i]).attr('href')}`);
+
+            let $$ = cheerio.load(article);
+            let text = $$('#content').text();
+            let link = $$('#mp3').attr('href');
+
+            audioList.push({
+                title,
+                text,
+                link
+            });
+        }
+    }
+
+    for (let i = 0; i < audioList.length; i++) {
+        console.log(`${(i / (audioList.length - 1)) * 100}%`);
+        let { title, text, link } = audioList[i];
+        fs.mkdirSync(`./dist/${title}  ${i}`);
+        let buffer = await getBuffer(link);
+        fs.writeFileSync(`${dir}/audio.mp3`, buffer);
+        fs.writeFileSync(`${dir}/text.txt`, text, 'utf8');
+    }
+}
+
+main();
